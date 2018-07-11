5df49f0cf64c7812abe6e228d8ff3b8da0e56cef
MunifTanjim <muniftanjim@gmail.com>
2018-07-12 02:37:34 +0600

    tweak src/scripts/helpers

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const path = require('path')
+const mkdirp = require('mkdirp')
 
 const readFile = dir => {
   return fs.readFileSync(dir)
 }
 
 const writeFile = (dir, data) => {
+  mkdirp.sync(path.dirname(dir))
+
   return fs.writeFileSync(dir, data)
 }
 
